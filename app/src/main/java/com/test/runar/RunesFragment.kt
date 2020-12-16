a061f906e17bd4987be6162cb2466176b0c11b9e
sokolovvova <vova.sokolov.1997@gmail.com>
2020-12-16 17:32:09 +0300

    Project created and "Main Screen"

---
+++ app/src/main/java/com/test/runar/RunesFragment.kt
@@ -0,0 +1,20 @@
+package com.test.runar
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+
+class RunesFragment: Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_runes,container,false)
+        return view
+    }
+}
