efa708c092de4df2a795c3b5a921547057f0f445
sokolovvova <vova.sokolov.1997@gmail.com>
2021-01-04 18:29:07 +0300

    hide bottom nav bar in descr, tint background, arrow added ( not work yet)

--- app/src/main/java/com/test/runar/ui/activity/MainActivity.kt
+++ app/src/main/java/com/test/runar/ui/activity/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.test.runar.ui.activity
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
@@ -20,5 +21,12 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationBar)
         val navController = findNavController(R.id.hostFragment)
         bottomNav.setupWithNavController(navController)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id ==R.id.layoutFragment){
+                bottomNav.visibility = View.VISIBLE
+            }
+            else
+                bottomNav.visibility = View.GONE
+        }
     }
 }
