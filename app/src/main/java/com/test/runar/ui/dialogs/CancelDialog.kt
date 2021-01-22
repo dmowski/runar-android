35cec2dcff8bac44b7b04531168634d5f071ba63
sokolovvova <vova.sokolov.1997@gmail.com>
2021-01-22 20:41:40 +0300

    Pop-ups

--- app/src/main/java/com/test/runar/ui/dialogs/CancelDialog.kt
+++ app/src/main/java/com/test/runar/ui/dialogs/CancelDialog.kt
@@ -1,15 +1,22 @@
 package com.test.runar.ui.dialogs
 
+import android.app.Activity
 import android.app.Dialog
+import android.content.Context
 import android.graphics.drawable.ColorDrawable
+import android.view.KeyEvent
 import android.view.Window
 import android.view.WindowManager
+import android.widget.FrameLayout
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.FragmentActivity
+import androidx.navigation.NavController
 import com.test.runar.R
 
-class CancelDialog {
-    fun showDialog(activity : FragmentActivity){
-        val dialog = Dialog(activity,android.R.style.ThemeOverlay)
+class CancelDialog<T : Activity>(private val navController: NavController,private val activity : T) {
+
+    fun showDialog(){
+        val dialog = Dialog(activity as Activity,android.R.style.ThemeOverlay)
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
         dialog.setCancelable(false)
         dialog.setContentView(R.layout.cancel_dialog_layout)
@@ -17,5 +24,18 @@ class CancelDialog {
         dialog.window?.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
         dialog.window?.setBackgroundDrawable(ColorDrawable(android.graphics.Color.TRANSPARENT));
         dialog.show()
+        dialog.setOnKeyListener { dialog, keyCode, _ ->
+            if(keyCode == KeyEvent.KEYCODE_BACK){
+                dialog.dismiss()
+            }
+            true
+        }
+        dialog.findViewById<ConstraintLayout>(R.id.dialog_element_left).setOnClickListener {
+            dialog.dismiss()
+        }
+        dialog.findViewById<ConstraintLayout>(R.id.dialog_element_right).setOnClickListener {
+            navController.navigate(R.id.layoutFragment)
+            dialog.dismiss()
+        }
     }
 }
