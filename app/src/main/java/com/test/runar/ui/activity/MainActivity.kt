2d4b655e43f92917517e3e1927b0043b9e8cd31b
sokolovvova <vova.sokolov.1997@gmail.com>
2021-01-23 14:09:37 +0300

    tralala

--- app/src/main/java/com/test/runar/ui/activity/MainActivity.kt
+++ app/src/main/java/com/test/runar/ui/activity/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.test.runar.ui.activity
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.test.runar.R
+import com.test.runar.presentation.viewmodel.MainViewModel
 import com.test.runar.ui.dialogs.CancelDialog
+import java.util.*
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
@@ -16,6 +20,8 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         if (savedInstanceState == null) {
             initBottomNavBar()
         }
+        val model: MainViewModel by viewModels()
+        model.identify()
     }
 
     private fun initBottomNavBar() {
@@ -32,6 +38,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     }
 
     override fun onBackPressed() {
+        Log.d("Log", UUID.randomUUID().toString())
         val navController = findNavController(R.id.hostFragment)
         when (navController.currentDestination?.id) {
             R.id.runesFragment -> navController.navigate(R.id.layoutFragment)
