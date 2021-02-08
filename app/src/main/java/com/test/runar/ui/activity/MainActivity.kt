64704c4073cfa15f24db59dbae838190e351ae5d
anastasiaShpakava <shpakavaa@gmail.com>
2021-02-08 18:10:25 +0300

    tab bar updating, added splash screen

--- app/src/main/java/com/test/runar/ui/activity/MainActivity.kt
+++ app/src/main/java/com/test/runar/ui/activity/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.test.runar.ui.activity
 
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -11,13 +12,14 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.test.runar.R
 import com.test.runar.presentation.viewmodel.MainViewModel
 import com.test.runar.ui.dialogs.CancelDialog
-import com.test.runar.ui.fragments.LayoutProcessingFragment
+
 import java.util.*
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
         if (savedInstanceState == null) {
             initBottomNavBar()
