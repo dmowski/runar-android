ecc875993bca23c2d37e5ba5bfe3fdd10d2ee4c3
artem <mel.arteym@gmail.com>
2021-02-11 11:59:28 +0300

    reformat code

--- app/src/main/java/com/test/runar/App.kt
+++ app/src/main/java/com/test/runar/App.kt
@@ -3,6 +3,7 @@ package com.test.runar
 import android.app.Application
 import com.test.runar.repository.SharedPreferencesRepository
 import com.test.runar.room.AppDB
+import com.test.runar.ui.activity.SplashActivity
 
 class App : Application() {
     override fun onCreate() {
