cbad9aa5dea1ee2284d2982f7cbb644c2e92bec5
artem <mel.arteym@gmail.com>
2021-02-11 11:59:28 +0300

    reformat code

--- app/src/main/java/com/test/runar/retrofit/utils/Status.kt
+++ app/src/main/java/com/test/runar/retrofit/utils/Status.kt
@@ -1,7 +1,7 @@
 package com.test.runar.retrofit.utils
 
-enum class Status {
-    SUCCESS,
-    ERROR,
-    LOADING
+sealed class Status {
+    object SUCCESS : Status()
+    object ERROR : Status()
+    object LOADING : Status()
 }
