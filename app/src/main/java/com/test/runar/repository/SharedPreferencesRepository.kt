7dac76effcb04a2bc9bf7bf9c151fa424b0a365d
artem <mel.arteym@gmail.com>
2021-02-11 10:55:49 +0300

    refactoring SharedPreferencesRepository.kt

--- app/src/main/java/com/test/runar/repository/SharedPreferencesRepository.kt
+++ app/src/main/java/com/test/runar/repository/SharedPreferencesRepository.kt
@@ -1,23 +1,23 @@
 package com.test.runar.repository
 
 import android.app.Application
+import android.content.SharedPreferences
 import androidx.preference.PreferenceManager
 import java.util.*
 
 class SharedPreferencesRepository(application: Application) {
-    val preferences = PreferenceManager.getDefaultSharedPreferences(application)
+    private val preferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(application)
 
-    fun getUserId(): String{
-        var userId: String =""
-        if(preferences.contains("Id")){
-            userId = preferences.getString("Id","").toString()
-        }
-        else{
+    val userId: String
+
+    init {
+        if (preferences.contains("Id")) {
+            userId = preferences.getString("Id", "").toString()
+        } else {
             val editor = preferences.edit()
             userId = UUID.randomUUID().toString()
-            editor.putString("Id",userId)
+            editor.putString("Id", userId)
             editor.apply()
         }
-        return userId
     }
 }
