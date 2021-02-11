c92a6155422f6c51c30e649879df94f2d30111c2
artem <mel.arteym@gmail.com>
2021-02-11 11:59:28 +0300

    reformat code

--- app/src/main/java/com/test/runar/retrofit/RetrofitClient.kt
+++ app/src/main/java/com/test/runar/retrofit/RetrofitClient.kt
@@ -2,15 +2,15 @@ package com.test.runar.retrofit
 
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
 
 object RetrofitClient {
     private const val TestServer ="https://runar-testing.herokuapp.com/"
     private const val MainServer ="https://runar-main.herokuapp.com/"
 
-    val retrofitClient = Retrofit.Builder()
+    private val retrofitClient: Retrofit = Retrofit.Builder()
         .baseUrl(TestServer+"api/v1/")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
-    val apiInterface = retrofitClient.create(BackendApiInterface::class.java)
+
+    val apiInterface: BackendApiInterface = retrofitClient.create(BackendApiInterface::class.java)
 }
