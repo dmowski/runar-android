2ab43f35425c6c4d9ef40fff8fcc21ddbfe2092b
Vladimir Sokolov <vova.sokolov.1997@gmail.com>
2021-02-15 16:56:10 +0300

    viewModel for description screen

---
+++ app/src/main/java/com/test/runar/presentation/viewmodel/DescriptionViewModel.kt
@@ -0,0 +1,40 @@
+package com.test.runar.presentation.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.test.runar.extensions.SingleLiveEvent
+import com.test.runar.model.LayoutDescriptionModel
+import com.test.runar.repository.DatabaseRepository
+import com.test.runar.repository.SharedDataRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class DescriptionViewModel : ViewModel() {
+
+    private var _selectedLayout = SingleLiveEvent<LayoutDescriptionModel>()
+    private var _fontSize = MutableLiveData<Float>()
+    val selectedLayout: LiveData<LayoutDescriptionModel> = _selectedLayout
+    val fontSize: LiveData<Float> = _fontSize
+
+    init {
+        _fontSize.postValue(SharedDataRepository.fontSize)
+    }
+
+
+    fun getLayoutDescription(id: Int) {
+        CoroutineScope(Dispatchers.IO).launch {
+            _selectedLayout.postValue(DatabaseRepository.getLayoutDetails(id))
+        }
+    }
+
+    fun notShowSelectedLayout(id: Int) {
+        CoroutineScope(Dispatchers.IO).launch {
+            DatabaseRepository.notShow(id)
+        }
+    }
+
+
+}
