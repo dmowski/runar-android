ac487386e7bc650525f0e973a27a36473c1088f2
Vladimir Sokolov <vova.sokolov.1997@gmail.com>
2021-02-16 14:38:28 +0300

    split vm

---
+++ app/src/main/java/com/test/runar/repository/RuneDataRepository.kt
@@ -0,0 +1,14 @@
+package com.test.runar.repository
+
+import android.content.Context
+import android.content.res.Resources
+import android.graphics.Paint
+import android.graphics.Rect
+import androidx.core.content.res.ResourcesCompat
+import com.test.runar.R
+
+object RuneDataRepository {
+
+    fun init(context: Context) {
+    }
+}
