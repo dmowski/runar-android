6051afe88bd05d3eaf7cb6809271e3de57cc5143
sokolovvova <vova.sokolov.1997@gmail.com>
2021-02-20 13:46:52 +0300

    fix font size

--- app/src/main/java/com/test/runar/ui/fragments/LibraryFragment.kt
+++ app/src/main/java/com/test/runar/ui/fragments/LibraryFragment.kt
@@ -30,8 +30,16 @@ class LibraryFragment : Fragment(R.layout.fragment_library) {
 
         viewModel.fontSize.observe(viewLifecycleOwner){ textSize->
             binding.itemHeader1.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize)
+            binding.itemHeader2.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize)
+            binding.itemHeader3.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize)
+            binding.itemHeader4.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize)
+            binding.itemHeader5.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize)
             val secondTextSize = (textSize*0.8).toFloat()
             binding.itemText1.setTextSize(TypedValue.COMPLEX_UNIT_PX, secondTextSize)
+            binding.itemText2.setTextSize(TypedValue.COMPLEX_UNIT_PX, secondTextSize)
+            binding.itemText3.setTextSize(TypedValue.COMPLEX_UNIT_PX, secondTextSize)
+            binding.itemText4.setTextSize(TypedValue.COMPLEX_UNIT_PX, secondTextSize)
+            binding.itemText5.setTextSize(TypedValue.COMPLEX_UNIT_PX, secondTextSize)
         }
     }
 }
