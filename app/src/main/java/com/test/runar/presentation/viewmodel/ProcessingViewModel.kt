018b42569b6cc06555cf91d1a79bee5cbd13ef38
sokolovvova <vova.sokolov.1997@gmail.com>
2021-02-21 19:25:10 +0300

    remove autosize tw from processing screen

--- app/src/main/java/com/test/runar/presentation/viewmodel/ProcessingViewModel.kt
+++ app/src/main/java/com/test/runar/presentation/viewmodel/ProcessingViewModel.kt
@@ -1,9 +1,11 @@
 package com.test.runar.presentation.viewmodel
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.test.runar.extensions.SingleLiveEvent
 import com.test.runar.repository.DatabaseRepository
+import com.test.runar.repository.SharedDataRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -12,6 +14,7 @@ class ProcessingViewModel : ViewModel() {
 
     private var _layoutName = SingleLiveEvent<String>()
     val layoutName : LiveData<String> = _layoutName
+    val fontSize: LiveData<Float> = MutableLiveData(SharedDataRepository.fontSize)
 
 
     fun getLayoutName(id: Int) {
