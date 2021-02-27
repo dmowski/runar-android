322c3670d7b4f9aa64b4f88277ac319bbbca168b
sokolovvova <vova.sokolov.1997@gmail.com>
2021-02-27 23:33:38 +0300

    compose top bar done

--- app/src/main/java/com/test/runar/ui/fragments/LibraryFragment.kt
+++ app/src/main/java/com/test/runar/ui/fragments/LibraryFragment.kt
@@ -12,17 +12,19 @@ import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.*
 import androidx.compose.ui.semantics.SemanticsProperties.Text
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.test.runar.R
@@ -91,13 +93,20 @@ fun Bars() {
                 title = { Text(text = "Библиотека") },
                 backgroundColor = colorResource(id = R.color.library_top_bar),
                 modifier = Modifier.aspectRatio(5.8f, false),
-                contentColor = colorResource(id = R.color.library_top_bar_header)
+                contentColor = colorResource(id = R.color.library_top_bar_header),
+                actions = {
+                    listOf(
+                        Icon(
+                            ImageVector.vectorResource(id = R.drawable.ic_library_top_fav),
+                            "Избранное",
+                            modifier = Modifier.padding(16.dp)
+                        )
+                    )
+                }
             )
         },
         backgroundColor = Color(0x00000000)
-    ) {
-
-    }
+    ) {}
 }
 
 @Composable
