b142cbfef59bb9b8168e52fcfee61376d3c2b5e5
sokolovvova <vova.sokolov.1997@gmail.com>
2021-02-27 19:14:56 +0300

    top bar compose

--- app/src/main/java/com/test/runar/ui/fragments/LibraryFragment.kt
+++ app/src/main/java/com/test/runar/ui/fragments/LibraryFragment.kt
@@ -8,9 +8,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.SemanticsProperties.Text
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.test.runar.R
@@ -57,8 +66,52 @@ class LibraryFragment : Fragment() {
     ): View? {
         return ComposeView(requireContext()).apply {
             setContent {
-                Text(text = "Hello world.")
+                TopBar()
             }
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TopBar()
+}
+
+@Composable
+fun TopBar() {
+    TopAppBar(
+        title = { Text(text = "Библиотека")},
+        backgroundColor = colorResource(id = R.color.library_top_bar)
+    )
+}
+
+@Composable
+fun FirstMenuItem() {
+    Row(
+        Modifier
+            .aspectRatio(3.8f, true)
+            .background(Color.Green)
+    ) {
+        Box(
+            Modifier
+                .fillMaxSize()
+                .weight(16f)
+                .background(Color.Blue)
+        )
+        Row(
+            Modifier
+                .fillMaxSize()
+                .weight(377f)
+                .background(Color.Gray)
+        ) {
+
+        }
+        Box(
+            Modifier
+                .fillMaxSize()
+                .weight(21f)
+                .background(Color.Yellow)
+        )
     }
 }
