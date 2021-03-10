0cf1979c7c70a28a207077166af04f0e8cef8a2d
sokolovvova <vova.sokolov.1997@gmail.com>
2021-03-10 19:42:11 +0300

    receiving data from backend and convert to room object

--- app/src/main/java/com/test/runar/repository/BackendRepository.kt
+++ app/src/main/java/com/test/runar/repository/BackendRepository.kt
@@ -1,6 +1,7 @@
 package com.test.runar.repository
 
 import com.test.runar.RunarLogger
+import com.test.runar.converters.DataClassConverters
 import com.test.runar.retrofit.RetrofitClient
 import com.test.runar.retrofit.UserInfo
 import kotlinx.coroutines.CoroutineScope
@@ -26,6 +27,26 @@ object BackendRepository {
                 RunarLogger.logDebug("Identification http error")
             } catch (e: Throwable) {
                 RunarLogger.logDebug("Identification some strange error")
+            }
+        }
+    }
+    fun getLibraryData(lang: String){
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val response = RetrofitClient.apiInterface.getLibraryData(lang)
+                withContext(Dispatchers.Main) {
+                    if (response.isSuccessful) {
+                        RunarLogger.logDebug("Library success: "+ response.message().toString())
+                        val convertedResult = DataClassConverters.LibRespToItems(response.body()!!)
+                        for(item in convertedResult) RunarLogger.logDebug(item.toString())
+                    } else {
+                        RunarLogger.logDebug("Library not success: "+ response.code().toString())
+                    }
+                }
+            } catch (e: HttpException) {
+                RunarLogger.logDebug("Library http error")
+            } catch (e: Throwable) {
+                RunarLogger.logDebug("Library some strange error")
             }
         }
     }
---
+++ app/src/main/java/com/test/runar/retrofit/LibraryResponse.kt
@@ -0,0 +1,11 @@
+package com.test.runar.retrofit
+
+data class LibraryResponse(
+    val childIds : List<String>? = null,
+    val _id: String? = null,
+    val imageUrl: String? = null,
+    val title: String? =null,
+    val sortOrder: Int? = null,
+    val content: String? =null,
+    val type: String? = null
+)
