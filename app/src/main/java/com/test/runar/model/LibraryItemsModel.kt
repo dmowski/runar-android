0d735477b4e9293393a18bb52f7bcd65615c670c
sokolovvova <vova.sokolov.1997@gmail.com>
2021-03-10 18:00:40 +0300

    first working version of db

--- app/src/main/java/com/test/runar/model/LibraryItemsModel.kt
+++ app/src/main/java/com/test/runar/model/LibraryItemsModel.kt
@@ -3,16 +3,22 @@ package com.test.runar.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.TypeConverters
+import com.test.runar.room.LibraryConverter
 
 @Entity(tableName = "library")
+@TypeConverters(LibraryConverter::class)
 data class LibraryItemsModel(
-    var icon: String?,
-    var title: String?,
-    var text: String?,
-    @ColumnInfo(name = "view_type")
-    var typeView: String?,
     @PrimaryKey
-    var id: Int?,
-    @ColumnInfo(name = "parent_id")
-    var parentId: Int?
-)
+    var id: String,
+    var childs: List<String>?,
+    var content: String?,
+    var title: String?,
+    @ColumnInfo(name = "image_url")
+    var imageUrl: String?,
+    @ColumnInfo(name = "sort_order")
+    var sortOrder: Int?,
+    var type: String?,
+){
+    constructor() : this("null", listOf(),null,null,null,null,null)
+}
---
+++ app/src/main/java/com/test/runar/room/LibraryConverter.kt
@@ -0,0 +1,17 @@
+package com.test.runar.room
+
+import androidx.room.TypeConverter
+import java.util.stream.Collectors
+
+class LibraryConverter {
+
+    @TypeConverter
+    fun fromChilds(items: List<String>?): String?{
+        return items?.stream()?.collect(Collectors.joining(","))
+    }
+
+    @TypeConverter
+    fun toChilds(data: String?) : List<String>?{
+        return data?.split("|") ?: listOf()
+    }
+}
