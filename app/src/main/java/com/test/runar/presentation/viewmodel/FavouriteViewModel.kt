2bc0a9ae1f9bd187ea43c773a3215f0268b55f40
sokolovvova <vova.sokolov.1997@gmail.com>
2021-03-13 14:12:13 +0300

    save date correction

--- app/src/main/java/com/test/runar/presentation/viewmodel/FavouriteViewModel.kt
+++ app/src/main/java/com/test/runar/presentation/viewmodel/FavouriteViewModel.kt
@@ -3,16 +3,15 @@ package com.test.runar.presentation.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.test.runar.extensions.SingleLiveEvent
 import com.test.runar.model.FavUserLayoutModel
-import com.test.runar.model.LayoutDescriptionModel
-import com.test.runar.model.LibraryItemsModel
 import com.test.runar.model.UserLayoutModel
 import com.test.runar.repository.DatabaseRepository
 import com.test.runar.repository.SharedDataRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
 
 class FavouriteViewModel: ViewModel() {
     val fontSize: LiveData<Float> = MutableLiveData(SharedDataRepository.fontSize)
@@ -32,7 +31,7 @@ class FavouriteViewModel: ViewModel() {
         val correctData = mutableListOf<FavUserLayoutModel>()
         for(item in favList){
             val correctItem = FavUserLayoutModel(
-                header = item.saveDate.toString(),
+                header = dateCorrection(item.saveDate),
                 content = textCorrection(item.interpretation),
                 imgId = null,
                 id = item.id
@@ -48,10 +47,20 @@ class FavouriteViewModel: ViewModel() {
             newText = text
                 .replace("<bf>","")
                 .replace("</bf>","")
+                .replace("<br>"," ")
         }
         else newText ="WTF"
-        newText = newText.substring(0,60)
+        newText = newText.substring(0,52)
         newText+="..."
         return newText
+    }
+
+    private fun dateCorrection(time: Long?): String{
+        if(time!=null){
+            val sdf = SimpleDateFormat("dd.MM.yyyy HH:mm")
+            val netDate = Date(time*1000)
+            return sdf.format(netDate)
+        }
+        else return "WTF"
     }
 }
