30c750cd049445ad53bff52b2b3b420ab6e501f2
sokolovvova <vova.sokolov.1997@gmail.com>
2021-04-13 13:43:37 +0300

    return autosize tw in main screen

--- app/src/main/java/com/tnco/runar/ui/fragments/LayoutFragment.kt
+++ app/src/main/java/com/tnco/runar/ui/fragments/LayoutFragment.kt
@@ -33,7 +33,7 @@ class LayoutFragment : Fragment(R.layout.fragment_layouts), View.OnClickListener
         setClickListenerOnRuneLayouts()
 
         viewModel.fontSize.observe(viewLifecycleOwner){fontSize->
-            val namesFontSize=(fontSize*1.6).toFloat()
+           /* val namesFontSize=(fontSize*1.6).toFloat()
             binding.firstLayoutTw.setTextSize(TypedValue.COMPLEX_UNIT_PX, namesFontSize)
             binding.secondLayoutTw.setTextSize(TypedValue.COMPLEX_UNIT_PX, namesFontSize)
             binding.thirdLayoutTw.setTextSize(TypedValue.COMPLEX_UNIT_PX, namesFontSize)
@@ -41,7 +41,7 @@ class LayoutFragment : Fragment(R.layout.fragment_layouts), View.OnClickListener
             binding.fifthLayoutTw.setTextSize(TypedValue.COMPLEX_UNIT_PX, namesFontSize)
             binding.sixthLayoutTw.setTextSize(TypedValue.COMPLEX_UNIT_PX, namesFontSize)
             binding.seventhLayoutTw.setTextSize(TypedValue.COMPLEX_UNIT_PX, namesFontSize)
-            binding.eightLayoutTw.setTextSize(TypedValue.COMPLEX_UNIT_PX, namesFontSize)
+            binding.eightLayoutTw.setTextSize(TypedValue.COMPLEX_UNIT_PX, namesFontSize)*/
         }
     }
 
