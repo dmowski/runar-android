d0c20ea7e26fc34e77d917645e64959ec1de231d
sokolovvova <vova.sokolov.1997@gmail.com>
2021-05-13 14:45:08 +0300

    affirm+layouts default db (ru)

--- app/src/main/java/com/tnco/runar/ui/fragments/LayoutDescriptionFragment.kt
+++ app/src/main/java/com/tnco/runar/ui/fragments/LayoutDescriptionFragment.kt
@@ -51,7 +51,7 @@ class LayoutDescriptionFragment : Fragment(R.layout.fragment_layout_description)
                 if (layoutDescriptionModel != null) {
                     binding.descriptionHeaderFrame.text = layoutDescriptionModel.layoutName
                     binding.descriptionTextView.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize)
-                    binding.descriptionTextView.text = layoutDescriptionModel.layoutDescription
+                    binding.descriptionTextView.text = layoutDescriptionModel.layoutDescription+"\n"
                     val headerTextSize = (textSize*3).toFloat()
                     val buttonTextSize = (textSize*1.65).toFloat()
                     val checkBoxTextSize = (textSize*0.8).toFloat()
