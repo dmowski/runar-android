bead56f8667f3c68edf1028f20975ec1dec56c47
RybinJD1 <alexfisher2013@gmail.com>
2022-07-13 10:28:06 +0200

    fix: change coroutine scope to IO for db method

--- app/src/main/java/com/tnco/runar/ui/viewmodel/GeneratorStartViewModel.kt
+++ app/src/main/java/com/tnco/runar/ui/viewmodel/GeneratorStartViewModel.kt
@@ -4,15 +4,18 @@ import androidx.lifecycle.*
 import com.tnco.runar.model.RunesItemsModel
 import com.tnco.runar.repository.DatabaseRepository
 import com.tnco.runar.repository.backend.BackendRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class GeneratorStartViewModel:ViewModel() {
+class GeneratorStartViewModel : ViewModel() {
 
-    var readRunes: LiveData<List<RunesItemsModel>> = DatabaseRepository.getRunesGenerator().asLiveData()
+    var readRunes: LiveData<List<RunesItemsModel>> =
+        DatabaseRepository.getRunesGenerator().asLiveData()
 
     var runesResponse: MutableLiveData<List<RunesItemsModel>> = MutableLiveData()
 
-    fun getRunes() = viewModelScope.launch {
-        runesResponse.value = BackendRepository.getRunes()
+    fun getRunes() = CoroutineScope(Dispatchers.IO).launch {
+        runesResponse.postValue(BackendRepository.getRunes())
     }
 }
