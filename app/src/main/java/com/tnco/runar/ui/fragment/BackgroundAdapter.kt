3369cf0529746620e6a583d9ebb5cd87c18bce37
Sergey Mikhovich <srgmikh1998@gmail.com>
2022-08-30 20:45:41 +0300

    Fixed too long generation of the result of getting background pictures

--- app/src/main/java/com/tnco/runar/ui/fragment/BackgroundAdapter.kt
+++ app/src/main/java/com/tnco/runar/ui/fragment/BackgroundAdapter.kt
@@ -1,23 +1,27 @@
 package com.tnco.runar.ui.fragments
 
-import android.graphics.Bitmap
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.ImageView
-import androidx.fragment.app.findFragment
+import android.widget.ProgressBar
 import androidx.recyclerview.widget.RecyclerView
 import com.tnco.runar.R
 import com.tnco.runar.retrofit.BackgroundInfo
 
-class BackgroundAdapter(val list: List<BackgroundInfo>, val fragment: GeneratorBackground) : RecyclerView.Adapter<BackgroundViewHolder>() {
+class BackgroundAdapter(
+    val selectBackground: (Int) -> Unit
+) : RecyclerView.Adapter<BackgroundViewHolder>() {
+
+    private val list = mutableListOf<BackgroundInfo>()
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BackgroundViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val itemView = inflater.inflate(R.layout.backgroung_generato_selected,parent,false)
         val holder = BackgroundViewHolder(itemView)
         itemView.setOnClickListener {
-            fragment.selectBackground(holder.adapterPosition)
+            selectBackground(holder.adapterPosition)
         }
         return holder
     }
@@ -26,11 +30,19 @@ class BackgroundAdapter(val list: List<BackgroundInfo>, val fragment: GeneratorB
         val img = holder.itemView.findViewById<ImageView>(R.id.backgroundImage)
         val checkBox = holder.itemView.findViewById<ImageView>(R.id.backgroundCheckBox)
         val frameLayout = holder.itemView.findViewById<FrameLayout>(R.id.background_framelayout)
+        val progressBar = holder.itemView.findViewById<ProgressBar>(R.id.progressBar)
 
+        progressBar.visibility = if (list[position].img != null) View.GONE else View.VISIBLE
         img.setImageBitmap(list[position].img)
         checkBox.visibility = if (list[position].isSelected) View.VISIBLE else View.GONE
         frameLayout.setBackgroundResource(if (list[position].isSelected) R.drawable.backround_background_selected
                                                       else R.drawable.backround_background)
+    }
+
+    fun updateData(list: List<BackgroundInfo>) {
+        this.list.clear()
+        this.list.addAll(list)
+        notifyDataSetChanged()
     }
 
     override fun getItemCount(): Int {
