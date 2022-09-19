e1a8ae056f66b2943783b3e4c7fc64b2d8e91636
RybinJD1 <alexfisher2013@gmail.com>
2022-09-19 17:50:52 +0200

    change runesList size

--- app/src/main/java/com/tnco/runar/ui/fragment/LayoutInitFragment.kt
+++ app/src/main/java/com/tnco/runar/ui/fragment/LayoutInitFragment.kt
@@ -3,6 +3,7 @@ package com.tnco.runar.ui.fragment
 import android.content.Context
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import android.util.TypedValue
 import android.view.View
 import android.widget.TextView
@@ -39,7 +40,7 @@ class LayoutInitFragment : Fragment(R.layout.fragment_layout_init), View.OnClick
     private lateinit var layoutFrame: ConstraintLayout
     private var fontSize: Float = 0f
     private var runeTable: Array<Array<Int>> = Array(7) { Array(3) { 0 } }
-    private var runesList: Array<Array<Int>> = Array(25) { Array(4) { 0 } }
+    private var runesList: Array<Array<Int>> = Array(25) { Array(2) { 0 } }
     private var layoutTable: Array<Int> = Array(9) { 0 }
     private var layoutId: Int = 0
     private var threadCounter = 0
@@ -386,53 +387,51 @@ class LayoutInitFragment : Fragment(R.layout.fragment_layout_init), View.OnClick
     }
 
     private fun runesArrayInit() {
-        runesList[0] = arrayOf(1, 2, 60, 30)//первые два столбца id рун вторые двух их процент удачи соответсвенно
-        runesList[1] = arrayOf(3, 4, 80, 20)
-        runesList[2] = arrayOf(5, 6, 20, 40)
-        runesList[3] = arrayOf(7, 8, 70, 20)
-        runesList[4] = arrayOf(9, 10, 70, 40)
-        runesList[5] = arrayOf(11, 12, 80, 20)
-        runesList[6] = arrayOf(13, 0, 90, 0)//0 когда нет обратной руны
-        runesList[7] = arrayOf(14, 15, 70, 40)
-        runesList[8] = arrayOf(16, 0, 30, 0)
-        runesList[9] = arrayOf(17, 18, 20, 20)
-        runesList[10] = arrayOf(19, 0, 10, 0)
-        runesList[11] = arrayOf(20, 0, 80, 0)
-        runesList[12] = arrayOf(21, 0, 20, 0)
-        runesList[13] = arrayOf(22, 23, 50, 40)
-        runesList[14] = arrayOf(24, 25, 70, 40)
-        runesList[15] = arrayOf(26, 0, 90, 0)
-        runesList[16] = arrayOf(27, 28, 80, 10)
-        runesList[17] = arrayOf(29, 30, 70, 30)
-        runesList[18] = arrayOf(31, 32, 60, 40)
-        runesList[19] = arrayOf(33, 34, 40, 40)
-        runesList[20] = arrayOf(35, 36, 50, 30)
-        runesList[21] = arrayOf(37, 0, 70, 0)
-        runesList[22] = arrayOf(38, 0, 90, 0)
-        runesList[23] = arrayOf(39, 40, 50, 40)
-        runesList[24] = arrayOf(41, 0, 50, 0)
+        runesList[0] = arrayOf(1, 2)
+        runesList[1] = arrayOf(3, 4)
+        runesList[2] = arrayOf(5, 6)
+        runesList[3] = arrayOf(7, 8)
+        runesList[4] = arrayOf(9, 10)
+        runesList[5] = arrayOf(11, 12)
+        runesList[6] = arrayOf(13, 0)//0 когда нет обратной руны
+        runesList[7] = arrayOf(14, 15)
+        runesList[8] = arrayOf(16, 0)
+        runesList[9] = arrayOf(17, 18)
+        runesList[10] = arrayOf(19, 0)
+        runesList[11] = arrayOf(20, 0)
+        runesList[12] = arrayOf(21, 0)
+        runesList[13] = arrayOf(22, 23)
+        runesList[14] = arrayOf(24, 25)
+        runesList[15] = arrayOf(26, 0)
+        runesList[16] = arrayOf(27, 28)
+        runesList[17] = arrayOf(29, 30)
+        runesList[18] = arrayOf(31, 32)
+        runesList[19] = arrayOf(33, 34)
+        runesList[20] = arrayOf(35, 36)
+        runesList[21] = arrayOf(37, 0)
+        runesList[22] = arrayOf(38, 0)
+        runesList[23] = arrayOf(39, 40)
+        runesList[24] = arrayOf(41, 0)
     }
 
     private fun getUniqueRune(): Int {
-        val minRuneLvl = preferencesRepository.minRuneLvl
-        var randomRunesList: Array<Array<Int>> = Array(3) { Array(6) { 0 } }
+        var randomRunesList: Array<Array<Int>> = Array(3) { Array(4) { 0 } }
         var randomRunesListSize = 0
         while (randomRunesListSize < 3) {
             val randomNumber = Random.nextInt(1, 42)
             if (layoutId == 2) {
                 for (i in 0..runesList.lastIndex) {
-                    if (runesList[i][0] == randomNumber) {
+                    if (runesList[i][0] == randomNumber) {//проверяет только из первого столбца руны,
+                        // т.к. обратные руны не сочитаются по докам 1,3,5,7,9,11,13,14,16,17,19,20,21,22,24,26,27,29,31,33,35,37,38,39,41
                         randomRunesList[randomRunesListSize] =
                             arrayOf(
                                 runesList[i][0],
                                 runesList[i][1],
-                                runesList[i][2],
-                                runesList[i][3],
                                 i,
                                 randomNumber
                             )
                         randomRunesListSize++
-                        runesList[i] = arrayOf(0, 0, 0, 0)
+                        runesList[i] = arrayOf(0, 0)
                         break
                     }
                 }
@@ -445,14 +444,12 @@ class LayoutInitFragment : Fragment(R.layout.fragment_layout_init), View.OnClick
                                 arrayOf(
                                     runesList[i][0],
                                     runesList[i][1],
-                                    runesList[i][2],
-                                    runesList[i][3],
-                                    i,
+                                    i,//номер массива runesList[i]
                                     randomNumber
                                 )
                             randomRunesListSize++
                             exit = true
-                            runesList[i] = arrayOf(0, 0, 0, 0)
+                            runesList[i] = arrayOf(0, 0)
                             break
                         }
                     }
@@ -461,15 +458,16 @@ class LayoutInitFragment : Fragment(R.layout.fragment_layout_init), View.OnClick
             }
         }
         var result = 0
-        for (n in 0..2) {
-            if (n == minRuneLvl) {
-                result = randomRunesList[n][5]
+        for (n in 0..2) {//перебор 3 массивов выбранных рандомно
+            if (n == 0) {//берется первый массив
+                result = randomRunesList[n][3]//=randomNumber из массива randomRunesList
             } else {
-                for (i in 0..3) {
-                    runesList[randomRunesList[n][4]][i] = randomRunesList[n][i]
-                }
+                for (i in 0..1) {// i- первый и второй символ массива runesList[][]
+                    runesList[randomRunesList[n][2]][i] = randomRunesList[n][i]//сетается обратно те же значения в массивы которые не использовались
+                }           //randomRunesList[n][2] = номер массива runesList[k]
             }
         }
+        Log.d("KEYKAK", "rune ID = $result")
         return result
     }
 
