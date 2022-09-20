6272333a23b21895a408e33d31e6832de1b8bdee
Sergey Mikhovich <srgmikh1998@gmail.com>
2022-09-20 20:47:53 +0300

    Create a separate fragment for the error layout

--- app/src/main/java/com/tnco/runar/ui/fragment/GeneratorFragment.kt
+++ app/src/main/java/com/tnco/runar/ui/fragment/GeneratorFragment.kt
@@ -7,32 +7,27 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import com.tnco.runar.R
 import com.tnco.runar.analytics.AnalyticsHelper
+import com.tnco.runar.databinding.FragmentLayoutGeneratorBinding
 import com.tnco.runar.enums.AnalyticsEvent
-import com.tnco.runar.databinding.FragmerntLayoutGeneratorBinding
-import com.tnco.runar.util.observeOnce
 import com.tnco.runar.ui.activity.MainActivity
 import com.tnco.runar.ui.viewmodel.MainViewModel
-import kotlinx.coroutines.launch
+import com.tnco.runar.util.observeOnce
 
 
 class GeneratorFragment : Fragment() {
     val viewModel: MainViewModel by viewModels()
 
-    private var _binding: FragmerntLayoutGeneratorBinding? = null
+    private var _binding: FragmentLayoutGeneratorBinding? = null
     private val binding get() = _binding!!
-
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmerntLayoutGeneratorBinding.inflate(inflater, container, false)
+        _binding = FragmentLayoutGeneratorBinding.inflate(inflater, container, false)
         viewModel.fontSize.observeOnce(this) {
             binding.tvToolbar.setTextSize(TypedValue.COMPLEX_UNIT_PX, (it * 1.35f))
         }
@@ -41,48 +36,30 @@ class GeneratorFragment : Fragment() {
             checkInternetConnection()
         }
 
-        binding.errorLayout.btnRetry.setOnClickListener {
-            hideInternetConnectionError()
-        }
         return binding.root
     }
 
     private fun checkInternetConnection() {
-        viewLifecycleOwner.lifecycleScope.launch {
-            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.CREATED) {
-                viewModel.isNetworkAvailable.observeOnce(viewLifecycleOwner) { status ->
-                    if (status) {
-                        activity?.supportFragmentManager?.beginTransaction()
-                            ?.replace(R.id.fragmentContainer, GeneratorStartFragment())
-                            ?.commit()
-                    } else {
-                        showInternetConnectionError()
-                    }
-                }
+        viewModel.isNetworkAvailable.observeOnce(viewLifecycleOwner) { status ->
+            if (status) {
+                moveToNextStep()
+            } else {
+                showInternetConnectionError()
             }
         }
     }
 
-    private fun showInternetConnectionError() = with(binding) {
-        generatorStav.visibility = View.GONE
-        formula.visibility = View.GONE
-        stav.visibility = View.GONE
-        textView2.visibility = View.GONE
-
-        errorLayout.errorMessage.visibility = View.VISIBLE
-        errorLayout.btnRetry.visibility = View.VISIBLE
+    private fun moveToNextStep() {
+        activity?.supportFragmentManager?.beginTransaction()
+            ?.replace(R.id.fragmentContainer, GeneratorStartFragment())
+            ?.commit()
     }
 
-    private fun hideInternetConnectionError() = with(binding) {
-        generatorStav.visibility = View.VISIBLE
-        formula.visibility = View.VISIBLE
-        stav.visibility = View.VISIBLE
-        textView2.visibility = View.VISIBLE
-
-        errorLayout.errorMessage.visibility = View.GONE
-        errorLayout.btnRetry.visibility = View.GONE
+    private fun showInternetConnectionError() {
+        activity?.supportFragmentManager?.beginTransaction()
+            ?.replace(R.id.fragmentContainer, ConnectivityErrorFragment())
+            ?.commit()
     }
-
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
