81751fb395f25a2c5d24c835fa9022a49164269c
Sergey <sergey.kamilyevich@gmail.com>
2022-12-13 17:48:52 +0700

    RUN-255 CI/CD: Connect and set up R8 https://runar.atlassian.net/browse/RUN-255 enable minify for release and debug variants, add annotation SerializedName to retrofit models

--- app/src/main/java/com/tnco/runar/data/remote/RunesResponse.kt
+++ app/src/main/java/com/tnco/runar/data/remote/RunesResponse.kt
@@ -1,8 +1,14 @@
 package com.tnco.runar.data.remote
 
+import com.google.gson.annotations.SerializedName
+
 data class RunesResponse(
+    @SerializedName("en")
     val en: En? = null,
+    @SerializedName("id")
     val id: Int? = null,
+    @SerializedName("imgUrl")
     val imgUrl: String? = null,
+    @SerializedName("ru")
     val ru: Ru? = null
 )
