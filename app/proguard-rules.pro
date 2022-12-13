1151e2f09a6b3e25b306f9355eb58f50147b49f4
sergey-i-am <71641200+sergey-i-am@users.noreply.github.com>
2022-12-13 19:01:33 +0700

    RUN-255 CI/CD: Connect and set up R8 https://runar.atlassian.net/browse/RUN-255 (#206)

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -19,3 +19,6 @@
 # If you keep the line number information, uncomment this to
 # hide the original source file name.
 #-renamesourcefileattribute SourceFile
+
+# enable for export the R8 configuration
+#-printconfiguration proguard_config.txt
--- app/src/main/java/com/tnco/runar/data/remote/LibraryResponse.kt
+++ app/src/main/java/com/tnco/runar/data/remote/LibraryResponse.kt
@@ -1,16 +1,30 @@
 package com.tnco.runar.data.remote
 
+import com.google.gson.annotations.SerializedName
+
 data class LibraryResponse(
+    @SerializedName("childIds")
     val childIds: List<String>? = null,
+    @SerializedName("_id")
     val _id: String? = null,
+    @SerializedName("imageUrl")
     val imageUrl: String? = null,
+    @SerializedName("title")
     val title: String? = null,
+    @SerializedName("sortOrder")
     val sortOrder: Int? = null,
+    @SerializedName("content")
     val content: String? = null,
+    @SerializedName("type")
     val type: String? = null,
+    @SerializedName("linkTitle")
     val linkTitle: String? = null,
+    @SerializedName("linkUrl")
     val linkUrl: String? = null,
+    @SerializedName("audioUrl")
     val audioUrl: String? = null,
+    @SerializedName("audioDuration")
     val audioDuration: Int? = null,
+    @SerializedName("tags")
     val tags: List<String>? = null
 )
