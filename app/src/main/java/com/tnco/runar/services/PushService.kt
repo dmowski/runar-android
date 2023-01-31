30d3c296c97517aff5c89e6b3e82b8b25bd371ea
Tehronshoh Ahatzoda <tehronkarimov55@mail.ru>
2023-01-31 16:57:34 +0300

    time diff changing notif_service

--- app/src/main/java/com/tnco/runar/services/PushService.kt
+++ app/src/main/java/com/tnco/runar/services/PushService.kt
@@ -4,23 +4,17 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.graphics.drawable.toBitmap
 import com.google.firebase.messaging.FirebaseMessagingService
-import com.google.firebase.messaging.RemoteMessage
 import com.tnco.runar.R
 import com.tnco.runar.RunarLogger
 import com.tnco.runar.repository.SharedPreferencesRepository
 import com.tnco.runar.ui.activity.MainActivity
-import java.text.SimpleDateFormat
-import java.time.LocalDate
-import java.util.*
 
 class PushService : FirebaseMessagingService() {
-
     private val preferencesRepository = SharedPreferencesRepository.get()
 
     override fun onNewToken(token: String) {
@@ -29,55 +23,17 @@ class PushService : FirebaseMessagingService() {
         super.onNewToken(token)
     }
 
-    override fun onMessageReceived(message: RemoteMessage) {
-        Log.d("KEYKAK", "onMessageReceived - ${message.notification?.body}")
-    }
-
     override fun handleIntent(intent: Intent?) {
         RunarLogger.logDebug("received message from server")
         Log.d("KEYKAK", "enter handle intent method")
-        Log.d("KEYKAK", "keySet - ${intent?.extras?.keySet()?.joinToString(" ")}")
-        Log.d(
-            "KEYKAK",
-            "gcm.notification.title - ${intent?.extras?.getString("gcm.notification.title")}"
-        )
         Log.d(
             "KEYKAK",
             "gcm.notification.body - ${intent?.extras?.getString("gcm.notification.body")}"
         )
         // crutch, for some reason the notification comes on the first start
         // if I open just only once, it will be problem
-        if (isShouldSend()) {
+        if (preferencesRepository.firstLaunch != 1) {
             sendNotification(intent)
-        }
-    }
-
-    private fun isShouldSend(): Boolean {
-        var isShouldSend = false
-        val currentDay = getCurrentDay()
-        val currentTime = System.currentTimeMillis()
-        val lastRunTime = preferencesRepository.lastDivination
-        val startTimeNotification = 11
-        val oneHour = 3600000L
-        val difference = oneHour * startTimeNotification
-        val isOk = (currentTime - lastRunTime) > difference
-        Log.d("KEYKAK", "$difference, $currentDay, ${currentTime - lastRunTime}")
-        if (isOk) {
-            Log.d("KEYKAK", "isOk=true")
-            isShouldSend = true
-        }
-        return isShouldSend
-    }
-
-    private fun getCurrentDay(): String {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            Log.d("KEYKAK", "build version: greater than O")
-            LocalDate.now().dayOfWeek.name.lowercase()
-        } else {
-            Log.d("KEYKAK", "build version: less than O")
-            val format = SimpleDateFormat("EEEE", Locale.ENGLISH)
-            val date = Date()
-            format.format(date).lowercase()
         }
     }
 
