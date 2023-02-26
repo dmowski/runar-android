9835af340bd7622d4868663ac11241fec82c30a4
sergey-i-am <sergey.kamilyevich@gmail.com>
2023-02-26 16:45:45 +0700

    Revert "RUN-330 Refactoring for new splash screen"
    This reverts commit daeff23a7291cb173c0642b64dd84318cb2be275.

--- app/src/androidTest/java/com/tnco/runar/ui/tests/OnboardTests.kt
+++ app/src/androidTest/java/com/tnco/runar/ui/tests/OnboardTests.kt
@@ -4,6 +4,7 @@ import androidx.test.ext.junit.rules.activityScenarioRule
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
 import com.tnco.runar.ui.activity.MainActivity
 import com.tnco.runar.ui.screen.OnboardScreen
+import com.tnco.runar.ui.screen.SplashScreen
 import org.junit.Rule
 import org.junit.Test
 
@@ -11,12 +12,32 @@ class OnboardTests : TestCase() {
     @get:Rule
     val activityRule = activityScenarioRule<MainActivity>()
 
-    private val onboardScreen = OnboardScreen()
+    @Test
+    fun testSplashVisible() = run {
+        step("Splash is visible") {
+            SplashScreen {
+                textView {
+                    isVisible()
+                }
+            }
+        }
+    }
 
     @Test
-    fun skipButtonTest() = run {
-        step("Click on skip button") {
-            onboardScreen.skipButton.click()
+    fun testOnboardVisibleAndClick() = run {
+        step("Onboard is visible") {
+            OnboardScreen {
+                skipButton {
+                    isVisible()
+                }
+            }
+        }
+        step("Skip Onboard") {
+            OnboardScreen {
+                skipButton {
+                    click()
+                }
+            }
         }
     }
 }
